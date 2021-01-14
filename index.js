96fbcbb0659d7f2d9b0c92e9ed12029b43ec3dec
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-14 11:03:31 -0800

    Initial commit with working server and DB connection.

---
+++ index.js
@@ -0,0 +1,31 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const typeDefs = gql`
+    type Query{
+      sayHi: String!  
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello World! '
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB Connected. ')
+        return server.listen({ port: 5000 })
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
