36e8e685667dd9963da792d2ac370908af68aaa9
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-14 17:16:12 -0800

    Adding: creating and deleting posts. Protecting posts.

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,16 +1,67 @@
+const { AuthenticationError } = require('apollo-server');
+
 const Post = require('../../Models/Post');
+const checkAuth = require('../../util/checkAuth');
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find();
+                // sort posts in desc order
+                const posts = await Post.find().sort({ createdAt: -1 })
                 return posts;
             }
             catch(error) {
                 throw new Error(error);
             }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+                if (post) {
+                    return post;
+                }
+                else {
+                    throw new Error('Post not found. ');
+                }
+            } 
+            catch(error) {
+                throw new Error(error);
+            }
         }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            // Investigate if token is valid or not. 
+            const user = checkAuth(context);
+            console.log(user);
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save()
+
+            return post;
+        },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context);
+      
+            try {
+              const post = await Post.findById(postId);
+              if (user.username === post.username) {
+                await post.delete();
+                return 'Post deleted successfully';
+              } else {
+                throw new AuthenticationError('Action not allowed');
+              }
+            } catch (err) {
+              throw new Error(err);
+            }
+          }
     }
 }
 
